5fcdcfec64afa16d4eb9fb8bc3dbcc4c20e2ad90
JEPTHE TAN (DOCU-IRIS-ISD-OOCLL/MNL) <TANJE5@OOCL.COM>
2022-08-31 13:45:44 +0800

    add directories

---
+++ java-collection/src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilter {
+
+    public List<Integer> filterEven(List<Integer> numbers) {
+
+        return numbers.stream().filter(number -> number % 2 == 0).collect(Collectors.toList());
+
+    }
+
+    public List<String> filterEndWithS(List<String> words) {
+
+        return null;
+
+    }
+
+    public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
+
+        return null;
+
+    }
+
+
+}
